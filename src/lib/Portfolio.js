38b00887489f8cb94f1dd118258c3ed7060c8e99
AntonEmery <antonemery@gmail.com>
2017-08-28 07:48:06 -0700

    fixed index.js

--- src/lib/Portfolio.js
+++ src/lib/Portfolio.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-import config from '../config/config'
+import config from '../config'
 
 import axios from 'axios'
 
